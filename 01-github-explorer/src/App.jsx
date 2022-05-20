85e02062e571673fe2bfd21fa346065f9f3e3ccf
Roberto Costa <roberto.araujo339@gmail.com>
2022-05-19 22:47:38 -0300

    RepositoryList and Item, list styling

--- 01-github-explorer/src/App.jsx
+++ 01-github-explorer/src/App.jsx
@@ -1,3 +1,4 @@
+import RepositoryList from './components/RepositoryList';
 import './styles/global.scss';
 
-export const App = () => <h1>Hello World</h1>;
+export const App = () => <RepositoryList />;
