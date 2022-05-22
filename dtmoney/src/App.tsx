02592445139d35022da187532f5a67b6f63c5afd
Roberto Costa <roberto.araujo339@gmail.com>
2022-05-22 11:52:59 -0300

    DT money created, basic layout and components

---
+++ dtmoney/src/App.tsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import { Dashboard } from './components/Dasboard';
+import Modal from 'react-modal';
+import { Header } from './components/Header';
+import { NewTransactionModal } from './components/NewTransactionModal';
+import { GlobalStyle } from './styles/global';
+
+Modal.setAppElement('#root');
+
+export const App = () => {
+  const [isNewTransactionsModalOpen, setIsNewTransactionsModalOpen] =
+    useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionsModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionsModalOpen(false);
+  }
+
+  return (
+    <>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+      <Dashboard />
+      <NewTransactionModal
+        isOpen={isNewTransactionsModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+      <GlobalStyle />
+    </>
+  );
+};
---
+++ dtmoney/src/components/Summary/index.tsx
@@ -0,0 +1,87 @@
+import { Container } from './styles';
+
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import totalImg from '../../assets/total.svg';
+import { useEffect, useState } from 'react';
+import { api } from '../../services/api';
+
+interface ITransaction {
+  id: number;
+  title: string;
+  amount: number;
+  type: string;
+  category: string;
+  createdAt: string;
+}
+
+export function Summary() {
+  const [transactions, setTransactions] = useState<ITransaction[]>([]);
+
+  useEffect(() => {
+    api.get('transactions').then((response) => {
+      setTransactions(response.data.transactions);
+    });
+  }, []);
+
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === 'deposit') {
+        acc.deposits += transaction.amount;
+        acc.total += transaction.amount;
+      } else {
+        acc.withdraws += transaction.amount;
+        acc.total -= transaction.amount;
+      }
+
+      return acc;
+    },
+    {
+      deposits: 0,
+      withdraws: 0,
+      total: 0,
+    }
+  );
+
+  return (
+    <Container>
+      <div>
+        <header>
+          <p>Entradas</p>
+          <img src={incomeImg} alt='Entradas' />
+        </header>
+        <strong>
+          {new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL',
+          }).format(summary.deposits)}
+        </strong>
+      </div>
+      <div>
+        <header>
+          <p>Saídas</p>
+          <img src={outcomeImg} alt='Saídas' />
+        </header>
+        <strong>
+          {summary.withdraws !== 0 ? '-' : ''}
+          {new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL',
+          }).format(summary.withdraws)}
+        </strong>
+      </div>
+      <div className='highlight-background'>
+        <header>
+          <p>Total</p>
+          <img src={totalImg} alt='Total' />
+        </header>
+        <strong>
+          {new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL',
+          }).format(summary.total)}
+        </strong>
+      </div>
+    </Container>
+  );
+}
