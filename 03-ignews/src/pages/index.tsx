eb415c372e0f6919a5e0af98f86e2d4f245f0ea6
Roberto Costa <roberto.araujo339@gmail.com>
2022-05-25 09:25:32 -0300

    ignews - Layout finished, SSR requests

--- 03-ignews/src/pages/index.tsx
+++ 03-ignews/src/pages/index.tsx
@@ -1,9 +1,59 @@
-const Home = () => {
-  return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
-  );
-};
+/* eslint-disable @next/next/no-img-element */
+import { GetStaticProps } from "next";
+import Head from "next/head";
+import { SubscribeButton } from "../components/SubscribeButton";
+import { stripe } from "../services/stripe";
+import styles from "./home.module.scss";
 
-export default Home;
+interface IHomeProps {
+	product: {
+		price_id: string;
+		amount: number;
+	};
+}
+
+export default function Home({ product: { price_id, amount } }: IHomeProps) {
+	return (
+		<>
+			<Head>
+				<title>Home | ig.news</title>
+			</Head>
+
+			<main className={styles.contentContainer}>
+				<section className={styles.hero}>
+					<span>👏 Hey, welcome</span>
+					<h1>
+						News about the <span>React</span> world.
+					</h1>
+					<p>
+						Get access to all the publications <br />
+						<span>
+							for
+							{amount}
+							month
+						</span>
+					</p>
+					<SubscribeButton />
+				</section>
+
+				<img src="/images/avatar.svg" alt="Girl coding" />
+			</main>
+		</>
+	);
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+	const price = await stripe.prices.retrieve("price_1L31JiDsVRmFsAwDRstZhzTP", {
+		expand: ["product"],
+	});
+
+	const product = {
+		priceId: price.id,
+		amount: new Intl.NumberFormat("en-US", {
+			style: "currency",
+			currency: "USD",
+		}).format(price.unit_amount / 100),
+	};
+
+	return { props: { product }, revalidate: 60 * 60 * 24 };
+};
